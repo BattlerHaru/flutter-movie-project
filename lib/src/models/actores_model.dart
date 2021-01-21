1d22e19de17eb2dcebdf1baf7509b548fb002430
V-Haru <kronk.sdance@gmail.com>
2021-01-21 13:57:27 -0600

    first commit

---
+++ lib/src/models/actores_model.dart
@@ -0,0 +1,62 @@
+class Cast {
+  List<Actor> actores = new List();
+
+  Cast.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+
+    jsonList.forEach((item) {
+      final actor = Actor.fromJsonMap(item);
+      actores.add(actor);
+    });
+  }
+}
+
+class Actor {
+  int castId;
+  String character;
+  String creditId;
+  int gender;
+  int id;
+  String name;
+  int order;
+  String profilePath;
+  String originalName;
+  double popularity;
+
+  Actor({
+    this.castId,
+    this.character,
+    this.creditId,
+    this.gender,
+    this.id,
+    this.name,
+    this.order,
+    this.profilePath,
+    this.originalName,
+    this.popularity,
+  });
+
+  Actor.fromJsonMap(Map<String, dynamic> json) {
+    castId = json['cast_id'];
+    character = json['character'];
+    creditId = json['credit_id'];
+    gender = json['gender'];
+    id = json['id'];
+    name = json['name'];
+    order = json['order'];
+    profilePath = json['profile_path'];
+    originalName = json['original_name'];
+    popularity = json['popularity'];
+  }
+
+  getFoto() {
+    if (profilePath == null ||
+        profilePath.isEmpty ||
+        profilePath == '' ||
+        profilePath == 'null') {
+      return 'https://cdn11.bigcommerce.com/s-auu4kfi2d9/stencil/59512910-bb6d-0136-46ec-71c445b85d45/e/933395a0-cb1b-0135-a812-525400970412/icons/icon-no-image.svg';
+    } else {
+      return 'https://image.tmdb.org/t/p/w500/$profilePath';
+    }
+  }
+}
---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+
+class DataSearch extends SearchDelegate {
+  String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
+
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam',
+    'Ironman',
+    'Capitan America'
+  ];
+
+  final peliculasRecienes = ['Spiderman', 'Capitan America'];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro appBar
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del appbar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // crea los resultaods que vamos a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Son sugerencias que aparecen cuando se escribe
+
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        if (snapshot.hasData) {
+          final peliculas = snapshot.data;
+
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
